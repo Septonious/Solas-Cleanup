806010190c65fff679c366be4a0fb818eec97cd8
ardemanse <septonious@proton.me>
2025-02-26 22:01:17 +0300

    Update fog.glsl

--- shaders/lib/atmosphere/fog.glsl
+++ shaders/lib/atmosphere/fog.glsl
@@ -42,7 +42,10 @@ void getNormalFog(inout vec3 color, in vec3 worldPos, in vec3 atmosphereColor, i
 	#ifdef OVERWORLD
     float fogDistanceFactor = mix(65.0, FOG_DISTANCE * (0.7 + timeBrightness * 0.3), caveFactor);
 	float fogDistance = min(192.0 / farPlane, 1.0) * (100.0 / fogDistanceFactor);
-	float fogVariableHeight = FOG_HEIGHT + texture2D(noisetex, (worldPos.xz + cameraPosition.xz + frameCounter * 0.04 * VC_SPEED) * 0.00002).b * 70.0;
+	float fogVariableHeight = FOG_HEIGHT;
+	#ifndef DISTANT_HORIZONS //Sorry DH simps, no luck for you this time
+		  fogVariableHeight += texture2D(noisetex, (worldPos.xz + cameraPosition.xz + frameCounter * 0.04 * VC_SPEED) * 0.00002).b * 70.0;
+	#endif
 	float fogAltitudeFactor = clamp(exp2(-max(cameraPosition.y - fogVariableHeight, 0.0) / exp2(FOG_HEIGHT_FALLOFF)), 0.0, 1.0);
 	float fogAltitude = clamp(exp2(-max(worldPos.y + cameraPosition.y - fogVariableHeight, 0.0) / exp2(FOG_HEIGHT_FALLOFF)), 0.0, 1.0);
 	float fogDensity = FOG_DENSITY * (2.0 - caveFactor) * (1.0 - pow(eBS, 0.1) * timeBrightness * 0.5);
